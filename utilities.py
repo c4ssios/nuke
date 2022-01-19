e9f4ed8cc3f431c9485722bb0d02ce53bce7d06a
c4ssios <c4ssios@gmail.com>
2022-01-19 03:02:25 -0800

    Create utilities.py

--- utilities.py
+++ utilities.py
@@ -1,10 +1,25 @@
+import os
+import nuke
+
+
 #Set all read Node to hold
 
 def setAllReadNodeToHold():
 
-readnodes = nuke.allNodes('Read')
-for readnode in readnodes:
-    nodeName = readnode.fullName()
-    nuke.toNode(nodeName).knob('after').setValue('hold')
+    readnodes = nuke.allNodes('Read')
+    for readnode in readnodes:
+        nodeName = readnode.fullName()
+        nuke.toNode(nodeName).knob('after').setValue('hold')
 
- 
+######################################################################################################################
+
+def createReadNodesFromFile():
+
+    path = "/Volumes/luma/stellarvortex/byPrefix/_MODEL/envTitan/txtr/mari/_output/images/color/"
+
+    for dirpath, dirnames, filenames in os.walk(path):
+        for f in filenames:
+            #print(os.path.join(dirpath, f))
+            filePath = os.path.join(dirpath, f)
+
+            nuke.nodes.Read(file=filePath)
